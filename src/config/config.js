e81bb7b99fc7afedaebfbda3f8caba378132a737
kanishk-479 <kanishkyadav0012@gmail.com>
2025-01-22 22:33:52 +0530

    after changing the server url

--- src/config/config.js
+++ src/config/config.js
@@ -1,1 +1,2 @@
-export const serverURL = "http://localhost:4321";
+// export const serverURL = "http://localhost:4321";
+export const serverURL = "https://crm-dashboard-backend.vercel.app"; // server
